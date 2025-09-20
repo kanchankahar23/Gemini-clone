0ec5666ae48868ed1d72604b5c9baf30c488c03e
kanchan kahar <kaharkanchan884@gmail.com>
2025-09-20 14:51:44 +0530

    style: improve layout and styling of Main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,17 +10,44 @@ const Main = () => {
                 <img src={assets.user_icon} />
             </div>
             <div className="main-container">
-             <div className="greet">
-                <p><span>Hello Kanchan..</span></p>
-                <p>How can I help you today?</p>
-             </div>
-             <div className="cards">
-                <div className="card">
-                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                <div className="greet">
+                    <p><span>Hello Kanchan..</span></p>
+                    <p>How can I help you today?</p>
                 </div>
-             </div>
+                <div className="cards">
+                    <div className="card">
+                        <p>Suggest beautiful places to see on an upcoming road trip</p>
+                        <img src={assets.compass_icon} />
+                    </div>
+
+                    <div className="card">
+                        <p>Briefly summarize this concept:urban planning </p>
+                        <img src={assets.bulb_icon} />
+                    </div>
+                    <div className="card">
+                        <p>Why umaria is famous for?</p>
+                        <img src={assets.message_icon} />
+                    </div>
+                    <div className="card">
+                        <p>Improve the readability of the following code</p>
+                        <img src={assets.code_icon} />
+                    </div>
+                </div>
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type="text" placeholder="Enter a prompt here" />
+                        <div>
+                            <img src={assets.gallery_icon} alt="" />
+                            <img src={assets.mic_icon} alt="" />
+                            <img src={assets.send_icon} alt="" />
+                        </div>
+                    </div>
+                       <p className="bottom-info">Gemini may display inaccurate info,including about people,so double-check  its response.Your privacy and Gemini App. </p>
+
+                </div>
             </div>
         </div>
+       
     )
 }
 export default Main
