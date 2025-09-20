0ee98513dbb802884c545c787b2e6dd58c9e68f9
kanchan kahar <kaharkanchan884@gmail.com>
2025-09-20 13:05:50 +0530

    refactor: rename project to vite-project and update dependencies
    - Changed project name from "gemini-clone" to "vite-project" in package.json.
    - Updated devDependencies to newer versions for better performance and compatibility.
    - Removed unused App.css styles and replaced App component content with Sidebar and Main components.
    - Added new assets for icons and created assets.js to manage them.
    - Created Sidebar and Main components with respective styles for layout and design.
    - Updated index.css to include a new font and simplified styles for better readability.

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './Main.css'
+import { assets } from "../../assets/assets";
+
+const Main = () => {
+    return (
+        <div className="main">
+            <div className="nav">
+                <p>Gemini</p>
+                <img src={assets.user_icon} />
+            </div>
+            <div className="main-container">
+             <div className="greet">
+                <p><span>Hello Kanchan..</span></p>
+                <p>How can I help you today?</p>
+             </div>
+             <div className="cards">
+                <div className="card">
+                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                </div>
+             </div>
+            </div>
+        </div>
+    )
+}
+export default Main
